124995dcabc77d424af51d2e74b73a0171320d66
ren0715 <r.s.izxo715@gmail.com>
2021-01-15 11:19:52 +0900

    dict

--- app2.py
+++ app2.py
@@ -8,5 +8,11 @@ def index2():
     message = "Hello!"
     return render_template('index2.html',message = message)
 
+@app.route('/dict_page')
+def dict_page():
+    tmp_dict ={}
+    tmp_dict['name']= 'Roj'
+    tmp_dict['City'] = 'Manila'
+    return render_template('dict_page.html',info = tmp_dict)
 if __name__ == "__main__":
     app.run()
